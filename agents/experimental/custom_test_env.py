4cae5a6b0260639c51683fe3743ef725e557ce76
mgfallen <minegoodfeeling@gmail.com>
2024-04-03 23:15:41 +0300

    [ENV][EXPERIMENTAL] added an agent with only 3 parameters of Postgres and also added DDPG model

--- agents/experimental/custom_test_env.py
+++ agents/experimental/custom_test_env.py
@@ -1,45 +1,26 @@
-import random
-
 import gymnasium as gym
 import numpy as np
-from gymnasium import spaces
 
 
-class CustomEnv(gym.Env):
-    """Custom Environment that follows gym interface."""
-
-    metadata = {"render_modes": ["human"], "render_fps": 30}
-
-    def __init__(self, initial_params, max_params):
-        super().__init__()
-        self.action_space = spaces.Box(low=np.array(initial_params), high=np.array(max_params), dtype=np.float32)
-        self.observation_space = spaces.Box(low=0, high=1, shape=(len(initial_params),), dtype=np.float32)
-
-        self.params = initial_params
-        self.max_params = max_params
+class DatabaseTuningEnv(gym.Env):
+    def __init__(self):
+        super(DatabaseTuningEnv, self).__init__()
+        self.action_space = gym.spaces.Box(low=np.array([0]), high=np.array([100]), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32)
+        self.shared_buffers = 50
+        self.latency = 100
+        self.throughput = 10
 
     def step(self, action):
-        self.params = action
+        self.shared_buffers += action
+        self.latency -= action * 0.1
+        self.throughput += action * 0.05
+        reward = self.throughput - self.latency
+        return np.array([self.shared_buffers, self.latency, self.throughput]), reward, False, {}
 
-        # query_time = execute_query(self.params)
-        query_time = random.random()
-
-        reward = -query_time
-
-        done = query_time >= 0.9
-
-        info = {}
-
-        return self.params / self.max_params, reward, done, False, info
-
-    def reset(self, seed=None, options=None):
-        if seed is not None:
-            np.random.seed(seed)
-        self.params = self.params
-        return self.params / self.max_params
-
-    def render(self):
-        pass
-
-    def close(self):
-        pass
+    def reset(self):
+        # Reset database parameters and metrics
+        self.shared_buffers = 50
+        self.latency = 100
+        self.throughput = 10
+        return np.array([self.shared_buffers, self.latency, self.throughput])
--- entry_points/experimental/custom_test_entry.py
+++ entry_points/experimental/custom_test_entry.py
@@ -1,4 +1,5 @@
-from stable_baselines3.common.env_checker import check_env
+from stable_baselines3 import DDPG
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 import agents.experimental.custom_test_env as env_maker
 
@@ -6,5 +7,16 @@ import agents.experimental.custom_test_env as env_maker
 if __name__ == '__main__':
     params = [1, 2, 3]
     max_params = [3, 3, 3]
-    env = env_maker.CustomEnv(params, max_params)
-    check_env(env)
+    env = DummyVecEnv([lambda: env_maker.DatabaseTuningEnv()])
+
+    model = DDPG('MlpPolicy', env, verbose=1)
+    model.learn(total_timesteps=10000)
+
+    # Evaluate trained agent
+    obs = env.reset()
+    for _ in range(100):
+        action, _ = model.predict(obs)
+        obs, reward, done, _ = env.step(action)
+        if done:
+            obs = env.reset()
+
