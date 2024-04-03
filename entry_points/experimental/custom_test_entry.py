e0eecc6ea5eb88847e75578233a620d99bd8e6ca
mgfallen <minegoodfeeling@gmail.com>
2024-04-04 00:02:14 +0300

    [ENV][EXPERIMENTAL] trying to make my own env works by creating env object in simple way

--- entry_points/experimental/custom_test_entry.py
+++ entry_points/experimental/custom_test_entry.py
@@ -1,16 +1,13 @@
 from stable_baselines3 import DDPG
-from stable_baselines3.common.vec_env import DummyVecEnv
 
 import agents.experimental.custom_test_env as env_maker
 
 
 if __name__ == '__main__':
-    params = [1, 2, 3]
-    max_params = [3, 3, 3]
-    env = DummyVecEnv([lambda: env_maker.DatabaseTuningEnv()])
+    env = env_maker.DatabaseTuningEnv()
 
     model = DDPG('MlpPolicy', env, verbose=1)
-    model.learn(total_timesteps=10000)
+    model.learn(total_timesteps=100)
 
     # Evaluate trained agent
     obs = env.reset()
